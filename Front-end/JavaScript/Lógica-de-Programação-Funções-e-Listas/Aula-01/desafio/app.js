0e0b4d8bb827990cccaea388d370cf6db2e63c22
Victor Carpinelli <117395073+carpinellx@users.noreply.github.com>
2025-02-27 13:35:33 -0300

    feat: complete challenges for Aula 01

---
+++ Front-end/JavaScript/Lógica-de-Programação-Funções-e-Listas/Aula-01/desafio/app.js
@@ -0,0 +1,31 @@
+// 1 Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
+
+let titulo = document.querySelector('h1');
+titulo.textContent = 'Hora do Desafio';
+
+// 2 Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+
+function verificarConsole() {
+  console.log('O botão foi clicado');
+}
+
+// 3 Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
+
+function verificarAlerta() {
+  alert('Eu amo JS');
+}
+
+// 4 Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
+
+function verificarPrompt() {
+  let cidade = prompt('Digite o nome de uma cidade do Brasil');
+  alert(`Estive em ${cidade} e lembrei de você`);
+}
+
+// 5 Ao clicar no botão soma, peça 2 números inteiros e exiba o resultado da soma em um alerta.
+
+function verificarSoma() {
+  let num1 = parseInt(prompt('Digite o primeiro número'));
+  let num2 = parseInt(prompt('Digite o segundo número'));
+  alert(`A soma dos números é ${num1 + num2}`);
+}
