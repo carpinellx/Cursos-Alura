8c49fce099d37709bc50942639ff0dfc57cecbc2
Victor Carpinelli <117395073+carpinellx@users.noreply.github.com>
2025-02-27 12:06:50 -0300

    feat: Manipulating texts

---
+++ Front-end/JavaScript/Lógica-de-Programação-Funções-e-Listas/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1'); // Seleciona o elemento h1
+titulo.innerHTML = 'Jogo do número secreto'; // Altera o conteúdo do h1
+
+let paragrafo = document.querySelector('p'); // Seleciona o elemento p
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10'; // Altera o conteúdo do p
