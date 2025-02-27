e43ec70a0f4365de439eed986dabc9fa0b96409e
Victor Carpinelli <117395073+carpinellx@users.noreply.github.com>
2025-02-27 12:39:29 -0300

    feat: testing the button

--- Front-end/JavaScript/Lógica-de-Programação-Funções-e-Listas/app.js
+++ Front-end/JavaScript/Lógica-de-Programação-Funções-e-Listas/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Jogo do número secreto'; // Altera o conteúdo do h1
 
 let paragrafo = document.querySelector('p'); // Seleciona o elemento p
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10'; // Altera o conteúdo do p
+
+function verificarChute() {
+    console.log('O botão foi clicado');
+}
