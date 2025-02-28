f1df651c94204c931d4b2ad59af0719a6dd39488
Victor Carpinelli <117395073+carpinellx@users.noreply.github.com>
2025-02-28 18:26:24 -0300

    add: function that checks if the guess is equal to the secret number

---
+++ Front-end/JavaScript/Lógica-de-Programação-Funções-e-Listas/Aula-02/desafio/app.js
@@ -0,0 +1,35 @@
+// 1 Criar uma função que exibe "Olá, mundo!" no console.
+
+function olaMundo() {
+  console.log("Olá, mundo!");
+} 
+
+// 2 Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+
+function olaNome(nome) {
+    console.log(`Olá, ${nome}!`);
+    }
+
+// 3 Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+
+function dobroNumero(numero) {
+    return numero * 2;
+}
+
+// 4 Criar uma função que recebe três números como parâmetros e retorna a média deles.
+
+function mediaNumeros(n1, n2, n3) {
+    return (n1 + n2 + n3) / 3;
+}
+
+// 5 Criar una o maior deles.ma função que recebe dois números como parâmetros e retor 
+
+function maiorNumero(n1, n2) {
+    return n1 > n2 ? n1 : n2; // Operador ternário (condição ? valor se verdadeiro : valor se falso) 
+} 
+
+// 6 Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+
+function numeroAoQuadrado(numero) {
+    return numero * numero;
+}
